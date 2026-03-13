525e533dcc507d56aceb689607107478ce57f530
asukix <asukix@gmail.com>
2026-03-13 22:40:19 +0100

    init

---
+++ Orderly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Orderly
+//
+//  Created by Phrakonkham Sengpraseuth on 2026. 03. 13..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OrderlyTests/OrderlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrderlyTests.swift
+//  OrderlyTests
+//
+//  Created by Phrakonkham Sengpraseuth on 2026. 03. 13..
+//
+
+import Testing
+@testable import Orderly
+
+struct OrderlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
